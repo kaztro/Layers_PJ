1a41ca1b79e712bf92149e967afbd769fffa0211
vegadevh <00148816@uca.edu.sv>
2020-06-23 13:57:22 -0600

    Base de proyecto

---
+++ ModuloEstudiantes/src/main/java/com/uca/capas/ModuloEstudiantesApplication.java
@@ -0,0 +1,13 @@
+package com.uca.capas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuloEstudiantesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModuloEstudiantesApplication.class, args);
+	}
+
+}
---
+++ ModuloEstudiantes/src/test/java/com/uca/capas/ModuloEstudiantesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uca.capas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuloEstudiantesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
