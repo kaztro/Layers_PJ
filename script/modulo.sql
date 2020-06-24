d0cbd888929dd85cedcca95b4a1af935e48d5f04
kaztro <00229017@uca.edu.sv>
2020-06-24 17:28:57 -0600

    La base de datos se finalizó

--- script/modulo.sql
+++ script/modulo.sql
@@ -2,9 +2,6 @@ CREATE DATABASE "MODULO"
     WITH 
     OWNER = postgres
     ENCODING = 'UTF8'
-    LC_COLLATE = 'Spanish_El Salvador.1252'
-    LC_CTYPE = 'Spanish_El Salvador.1252'
-    TABLESPACE = pg_default
     CONNECTION LIMIT = -1;
 
 CREATE TABLE DEPARTAMENTO(
@@ -23,7 +20,7 @@ CREATE TABLE USUARIO(
 	id_usuario SERIAL PRIMARY KEY NOT NULL,
 	username VARCHAR(50) NOT NULL,
 	password VARCHAR(50) NOT NULL,
-	enabled BOOL NOT NULL,
+	enabled_u BOOL NOT NULL,
 	nombre VARCHAR(50) NOT NULL,
 	apellido VARCHAR(50) NOT NULL,
 	fNac DATE NOT NULL,
@@ -53,7 +50,7 @@ CREATE TABLE CENTROS_ESCOLARES(
 	nombre VARCHAR(80) NOT NULL,
 	descripcion TEXT NOT NULL,
 	estado BOOL NOT NULL,
-    enabled BOOL NOT NULL,
+    enabled_ce BOOL NOT NULL,
 	id_municipio INTEGER NOT NULL,
 	FOREIGN KEY(id_municipio) REFERENCES MUNICIPIO(id_municipio)
 );
@@ -77,7 +74,7 @@ CREATE TABLE MATERIA(
 	id_materia SERIAL PRIMARY KEY NOT NULL,
 	materia VARCHAR(10) NOT NULL,
 	descripcion TEXT NOT NULL,
-    enabled BOOL NOT NULL
+    enabled_m BOOL NOT NULL
 );
 
 CREATE TABLE MATERIA_CURSADA(
