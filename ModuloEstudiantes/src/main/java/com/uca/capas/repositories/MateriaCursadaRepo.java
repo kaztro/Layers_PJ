ffff83dd5e81b5ee93e7cd731039364da52d41cc
vegadevh <00148816@uca.edu.sv>
2020-07-05 12:04:50 -0600

    Modificacion de Query para respuesta en DTO

--- ModuloEstudiantes/src/main/java/com/uca/capas/repositories/MateriaCursadaRepo.java
+++ ModuloEstudiantes/src/main/java/com/uca/capas/repositories/MateriaCursadaRepo.java
@@ -10,30 +10,30 @@ import com.uca.capas.domain.MateriaCursada;
 
 public interface MateriaCursadaRepo extends JpaRepository<MateriaCursada, Integer> {
 	
-	@Query(nativeQuery=true, value="SELECT e.nombres, e.apellidos, ma.aprobadas, COUNT(mc.nota) AS Reprobadas, sal.promedio "+
-			"FROM( "+
-				"SELECT mc.id_estudiante, COUNT(nota) AS Aprobadas "+
-				"FROM public.materia_cursada AS mc "+
-				"INNER JOIN public.estudiante AS e "+
-				"ON mc.id_estudiante = e.id_estudiante "+
-				"WHERE mc.nota >= 6 "+
-				"GROUP BY mc.id_estudiante "+
-			") AS ma "+
-			"INNER JOIN public.materia_cursada AS mc "+
-			"ON mc.id_estudiante = ma.id_estudiante "+
-			"INNER JOIN public.estudiante AS e "+
-			"ON ma.id_estudiante = e.id_estudiante "+
-			"INNER JOIN( "+
-				"SELECT e.id_estudiante, ROUND(SUM(mc.nota)/COUNT(mc.nota), 2) as promedio "+
-				"FROM public.estudiante AS e "+
-				"INNER JOIN public.materia_cursada as mc "+
-				"ON e.id_estudiante = mc.id_estudiante "+
-				"INNER JOIN public.materia AS m "+
-				"ON mc.id_materia = m.id_materia "+
-				"GROUP BY e.id_estudiante "+
-			") AS sal "+
-			"ON e.id_estudiante = sal.id_estudiante "+
-			"GROUP BY e.nombres, e.apellidos, ma.aprobadas, sal.promedio")
+	@Query(nativeQuery=true, value="SELECT e.nombres, e.apellidos, CASE WHEN ma.aprobadas IS NULL THEN 0 ELSE ma.aprobadas END, CASE WHEN ma.reprobadas IS NULL THEN 0 ELSE ma.reprobadas END, CASE WHEN ROUND(AVG(mc.nota),2) IS NULL THEN 0.0 ELSE ROUND(AVG(mc.nota),2) END AS promedio "+
+	"FROM( "+
+		"SELECT cru.id_estudiante, CASE WHEN COUNT(cru.nota) IS NULL THEN 0 ELSE COUNT(cru.nota) END AS Aprobadas, CASE WHEN fir.reprobadas IS NULL THEN 0 ELSE fir.reprobadas END AS Reprobadas "+
+		"FROM( "+
+			"SELECT mc.id_estudiante, CASE WHEN COUNT(mc.nota) IS NULL THEN 0 ELSE COUNT(mc.nota) end AS Reprobadas "+
+			"FROM public.materia_cursada AS mc "+
+			"FULL OUTER JOIN public.estudiante AS e "+
+			"ON mc.id_estudiante = e.id_estudiante "+
+			"WHERE mc.nota < 6 AND mc.nota >0 "+
+			"GROUP BY mc.id_estudiante "+
+		") AS fir "+
+		"FULL OUTER JOIN public.materia_cursada AS cru "+
+		"ON fir.id_estudiante = cru.id_estudiante "+
+		"FULL OUTER JOIN public.estudiante AS es "+
+		"ON fir.id_estudiante = es.id_estudiante "+
+		"WHERE cru.nota >= 6 AND cru.nota <=10 "+
+		"GROUP BY cru.id_estudiante, fir.Reprobadas "+
+	") AS ma "+
+	"FULL OUTER JOIN public.materia_cursada AS mc "+
+	"ON ma.id_estudiante = mc.id_estudiante "+
+	"FULL OUTER JOIN public.estudiante AS e "+
+	"ON mc.id_estudiante = e.id_estudiante "+
+	"GROUP BY e.id_estudiante, mc.id_estudiante, e.nombres, e.apellidos, ma.aprobadas, ma.reprobadas "+
+	"ORDER BY e.id_estudiante DESC")
 	public List<Object[]> registroDTO() throws DataAccessException;
 
 }
