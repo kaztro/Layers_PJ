b19d600ba72ae116dc9277504a6755639a53e0b8
vegadevh <00148816@uca.edu.sv>
2020-07-08 18:19:15 -0600

    Agregando funcionalidad crear centro escolar - fallos

--- ModuloEstudiantes/src/main/java/com/uca/capas/controller/AdminController.java
+++ ModuloEstudiantes/src/main/java/com/uca/capas/controller/AdminController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -212,26 +213,45 @@ public class AdminController {
 	
 	// Add - CE
 	@RequestMapping("/admin/ingresarCE")
-	public ModelAndView ingresarCE(@Valid CentroEscolar centroEscolar, BindingResult result, Model model) {
+	public ModelAndView ingresarCE(@Valid CentroEscolar centroEscolar, BindingResult result) {
 		ModelAndView mav = new ModelAndView();
-		if(result.hasErrors()) {
-			mav.setViewName("addCE");
-		} else {	
-			centroEscolarService.save(centroEscolar);
-			
 			List<Departamento> departamentos = null;
 			List<Municipio> municipios = null;
-			try { 
-				departamentos = departamentoService.findAll();
-				municipios = municipioService.findAll();
-			}catch(Exception e) {
-				e.printStackTrace();
-			}
+			departamentos = departamentoService.findAll();
+			municipios = municipioService.findAll();
 			
 			mav.addObject("departamentos", departamentos);
 			mav.addObject("municipios", municipios);
 			mav.addObject("centroEscolar", centroEscolar);
 			mav.setViewName("addCE");
+		
+		return mav;
+	}
+	
+	@RequestMapping("/admin/validar/ingresarCE")
+	public ModelAndView validarrCE(@Valid CentroEscolar centroEscolar, BindingResult result) {
+		ModelAndView mav = new ModelAndView();
+		if(result.hasErrors()) {
+			List<Departamento> departamentos = null;
+			List<Municipio> municipios = null;
+			departamentos = departamentoService.findAll();
+			municipios = municipioService.findAll();
+			
+			mav.addObject("departamentos", departamentos);
+			mav.addObject("municipios", municipios);
+			
+			mav.setViewName("addCE");
+		} else {	
+			List<CentroEscolar> cEscolares = null;
+			try {
+				cEscolares = centroEscolarService.findAll();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
+			mav.addObject("centroEscolar", new CentroEscolar());
+			mav.addObject("cEscolares", cEscolares);
+			mav.setViewName("listCEsc");
 		}
 		return mav;
 	}
